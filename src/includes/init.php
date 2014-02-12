8fc14965ccb3833a293ccf95afafcdf872580cfc
Charles Duprey <barbatos@urbanterror.info>
2014-02-12 18:56:33 +0100

    Ajout d'un fichier d'init avec connexion à la bdd

---
+++ src/includes/init.php
@@ -0,0 +1,37 @@
+<?php
+
+session_start();
+
+error_reporting(0);
+
+// On définit le charset et le timezone
+header('Content-Type: text/html; charset=UTF-8');
+mb_internal_encoding('UTF-8');
+date_default_timezone_set('Europe/Paris');
+ini_set('arg_separator.output', '&amp;');
+
+// On force la désactivation des magic quotes
+ini_set('magic_quotes_runtime', 0);
+
+require_once('fonctions.php');
+
+// Connexion à la base de données
+$db['user'] 	= "infoplus";
+$db['password'] = "infoplus";
+$db['dbname'] 	= "infoplus";
+$db['host'] 	= "barbatos.fr";
+$db['db'] 		= "mysql:host=".$db['host'].";dbname=".$db['dbname'].";charset=UTF8";
+
+$bdd = new PDO($db['db'], $db['user'], $db['password'], array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+if(!$bdd){
+	exit('Erreur de connexion à la base de données.');
+}
+
+unset($db);
+
+// Pas de messages à afficher ? On initialise quand même 
+// les variables afin de ne pas avoir de notices de php.
+if(!isset($_SESSION['errors'])){
+	$_SESSION['errors'] = array();
+	$_SESSION['messages'] = array();
+}
