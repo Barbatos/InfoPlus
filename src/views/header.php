1c6a683ba696ae09811f67857a888505ce2dfb58
Charles Duprey <barbatos@urbanterror.info>
2014-02-19 10:41:38 +0100

    Display the messages to the user

--- src/views/header.php
+++ src/views/header.php
@@ -1,5 +1,8 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 		<title><?= Page::$title ?></title>
 
 		<link href="http://bootstrapdocs.com/v2.3.2/docs/assets/css/bootstrap.css" rel="stylesheet">
@@ -10,3 +13,20 @@
 	    <![endif]-->
 	</head>
 	<body>
+
+	<?php 
+    if(isset($_SESSION['errors']) && !empty($_SESSION['errors'])){
+      foreach($_SESSION['errors'] as $e){
+        echo '<br /><br /><br /><div class="alert alert-error">'.$e['error'].'</div>';
+      }
+      $_SESSION['errors'] = array();
+    }
+
+    if(isset($_SESSION['messages']) && !empty($_SESSION['messages'])){
+      foreach($_SESSION['messages'] as $e){
+        echo '<br /><br /><br /><div class="alert alert-success">'.$e['message'].'</div>';
+      }
+      $_SESSION['messages'] = array();
+    }
+    ?>
+    
