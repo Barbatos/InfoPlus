73f998549119d8753632f7deb1bb21df48f15af6
Charles Duprey <barbatos@urbanterror.info>
2014-03-23 17:05:52 +0100

    Draw the recent news in the sidebar

--- src/views/header.php
+++ src/views/header.php
@@ -36,7 +36,28 @@
               <div id="logo">
                 <h1>InfoPlus</h1>
               </div>
-            
+              
+              <center>
+              <?php 
+              if(is_connected()) {
+              ?>
+              Hey! :) <br />
+              <a href="<?= WEBSITE_URL ?>logout/">Logout</a>
+
+              <?php 
+              }
+
+              else {
+              ?>
+
+              <a href="<?= WEBSITE_URL ?>connection/">Log In</a> <br />
+              <a href="<?= WEBSITE_URL ?>register/">Register</a> <br />
+
+              <?php 
+              }
+              ?>
+              </center>
+
             <!-- Search -->
               <section class="is-search">
                 <form method="post" action="#">
@@ -70,76 +91,21 @@
                   <h2>Recent News</h2>
                 </header>
                 <ul>
-                  <li><a href="#">Nothing happened</a></li>
-                  <li><a href="#">My Dearest Cthulhu</a></li>
-                  <li><a href="#">The Meme Meme</a></li>
-                  <li><a href="#">Now Full Cyborg</a></li>
-                  <li><a href="#">Temporal Flux</a></li>
+                  <?php 
+                  $stmt = $bdd->prepare('SELECT * FROM article ORDER BY dateAjout DESC');
+                  $stmt->execute();
+                  $data = $stmt->fetchAll(PDO::FETCH_OBJ);
+                  $stmt->closeCursor();
+
+                  foreach($data as $d) {
+                  ?>
+                  <li><a href="/article/<?= rewrite($d->titre) ?>"><?= $d->titre ?></a></li>
+                  <?php 
+                  }
+                  ?>
                 </ul>
               </section>
           
-            <!-- Calendar -->
-              <section class="is-calendar">
-                <div class="inner">
-                  <table>
-                    <caption>February 2013</caption>
-                    <thead>
-                      <tr>
-                        <th scope="col" title="Monday">M</th>
-                        <th scope="col" title="Tuesday">T</th>
-                        <th scope="col" title="Wednesday">W</th>
-                        <th scope="col" title="Thursday">T</th>
-                        <th scope="col" title="Friday">F</th>
-                        <th scope="col" title="Saturday">S</th>
-                        <th scope="col" title="Sunday">S</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td colspan="4" class="pad"><span>&nbsp;</span></td>
-                        <td><span>1</span></td>
-                        <td><span>2</span></td>
-                        <td><span>3</span></td>
-                      </tr>
-                      <tr>
-                        <td><span>4</span></td>
-                        <td><span>5</span></td>
-                        <td><a href="#">6</a></td>
-                        <td><span>7</span></td>
-                        <td><span>8</span></td>
-                        <td><span>9</span></td>
-                        <td><a href="#">10</a></td>
-                      </tr>
-                      <tr>
-                        <td><span>11</span></td>
-                        <td><span>12</span></td>
-                        <td><span>13</span></td>
-                        <td class="today"><a href="#">14</a></td>
-                        <td><span>15</span></td>
-                        <td><span>16</span></td>
-                        <td><span>17</span></td>
-                      </tr>
-                      <tr>
-                        <td><span>18</span></td>
-                        <td><span>19</span></td>
-                        <td><span>20</span></td>
-                        <td><span>21</span></td>
-                        <td><span>22</span></td>
-                        <td><a href="#">23</a></td>
-                        <td><span>24</span></td>
-                      </tr>
-                      <tr>
-                        <td><a href="#">25</a></td>
-                        <td><span>26</span></td>
-                        <td><span>27</span></td>
-                        <td><span>28</span></td>
-                        <td class="pad" colspan="3"><span>&nbsp;</span></td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-              </section>
-            
             <!-- Copyright -->
               <div id="copyright">
                 <p>
